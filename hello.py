338436c3cca28eb0295a9b24bd3263fe126d4712
Bao Nhi Thai <baonhithai15@gmail.com>
2023-02-23 14:11:04 -0500

    initial commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This program says hello and asks for my name
+
+print('Hello World')
+print('What is your name?')
+my_name = input()   # ask for their name
+print('It is good to meet you, ' + my_name)
+print('The length of your name is:')
+print(len(my_name))
+print('What is your age?')
+my_age = input()    # ask for their age
+print('You will be ' + str(int(my_age) + 1) + ' in a year.')
